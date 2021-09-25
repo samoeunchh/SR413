ad497080a2fbfafeee3ee1b2ace4aba60687bba7
samoeun chhay <samoeunchh@gmail.com>
2021-09-25 15:34:51 +0700

    Add project files.

---
+++ SR413/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SR413.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SR413.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
